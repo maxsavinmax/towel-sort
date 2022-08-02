58a4921ca72fd1b31d0532deffc4ca9f7cb086d6
maxsavinmax <savin.maksim.savin@gmail.com>
2022-08-02 22:32:12 +0300

    fix: task done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = [];
+  if (matrix === undefined || matrix.length == 0) {
+    return [];
+  } else { 
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 !== 0) {
+        arr.push(matrix[i].reverse());
+      } else {
+        arr.push(matrix[i]);
+      }
+    }
+  return arr.flat();
+}
 }
